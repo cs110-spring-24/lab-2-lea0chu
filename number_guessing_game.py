af8fba331d8c54fb716f3462d47fb5e447268999
lea0chu <111941235+lea0chu@users.noreply.github.com>
2024-02-08 21:38:32 -0800

    Update number_guessing_game.py
    🥲

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,39 +1,29 @@
-
 import random
 
-num = random.randint(1, 100)
-user = 0
-wins = 0
-high = 0
-low = 0
-
-while num != user:
-  user = int(input("Enter your guess: "))  
-  if user > num:
-    print(f"Too high.")
-    high += 1
-  elif user < num:
-    print(f"Too low.")
-    low += 1
-  else:
-    print("You got it!")
-    wins += 1
-    print(f"It took you {high+low} guesses to solve it!")
-    
-again = input("Run again?: ")
+def start_game():
+    computer_number = random.randint(0, 100)
+    guess = -1 
+    wins = 0
+    low = 0
+    high = 0
+    while guess != computer_number:
+        guess = int(input("Guess a number from 0 to 100: \n"))
+        if guess == computer_number:
+            print("You won!")
+            wins += 1
+            print(f"It took you {high+low} guesses to solve it!")
+        elif guess > computer_number:
+            print("Try a lower number!")
+            low += 1
+        else:
+            print("Try a higher number!")
+            high +=1
 while True:
-  if 'Yes' in again:
-    user = int(input("Enter your guess: "))
-    if user > num:
-      print(f"Too high.")
-      high += 1
-    elif user < num:
-      print(f"Too low.")
-      low += 1
-    elif user == num:
-      print("You got it!")
-      wins += 1
-      print(f"It took you {high+low} guesses to solve it!")
-  else: 
-    print("Goodbye")
-    break
+    restart = input ("Run again? ")
+    if "Yes"in restart:
+      start_game()
+    else:
+      print("Goodbye.")
+      break
+
+
